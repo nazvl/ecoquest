fc8b90f401872335f9e130777db84a991a5d7bdf
Vladimir N. <lismiroslav@yandex.ru>
2025-03-10 08:24:13 +0300

    Remove unnecessary React import from ScrollVelocityComponent and Recommendations for cleaner code

--- src/components/ScrollVelocityComponent.tsx
+++ src/components/ScrollVelocityComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from "react";
+import { useRef, useEffect, useState } from "react";
 
 // Описание параметров компонента
 interface ScrollVelocityComponentProps {
